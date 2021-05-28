b6d262dd9e4d53035d26cde7d78785a983af2e7f
barthuel <paulbarthuel@hotmail.fr>
2021-05-28 14:38:25 +0200

    feat(adding_create_real_estate_activity_ui): Adding the ui of CreateActivity

--- app/src/main/java/com/openclassrooms/realestatemanager/app/modules/main/views/RealEstateListFragment.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/app/modules/main/views/RealEstateListFragment.kt
@@ -1,14 +1,17 @@
 package com.openclassrooms.realestatemanager.app.modules.main.views
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import com.openclassrooms.realestatemanager.R
+import com.openclassrooms.realestatemanager.app.modules.realEstateCreate.RealEstateCreateActivity
 import com.openclassrooms.realestatemanager.databinding.FragmentRealEstateListBinding
 
 class RealEstateListFragment : Fragment() {
@@ -26,10 +29,34 @@ class RealEstateListFragment : Fragment() {
         
         return binding.root
     }
+    
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    
+        setupUI()
+    }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         if (detailFragmentLayout == null) {
             inflater.inflate(R.menu.drawer_list, menu)
         }
+    }
+    
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (detailFragmentLayout == null) {
+            when (item.itemId) {
+                R.id.menu_activity_creation -> {
+                    Intent(context, RealEstateCreateActivity::class.java).also { startActivity(it) }
+                }
+                R.id.menu_activity_search -> {
+                    // TODO intent for future searchActivity
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    
+    private fun setupUI() {
+        detailFragmentLayout = view?.rootView?.findViewById(R.id.detailFragment)
     }
 }
